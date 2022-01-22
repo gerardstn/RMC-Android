677e991aac836d66c50e230569094c48fb16b3ca
Gerard <g.sintnicolaas@student.avans.nl>
2022-01-22 21:53:39 +0100

    merged with current master, added randomsession method back in

--- app/src/main/java/avdinformatica/group1/rentmycar/utils/Helper.java
+++ app/src/main/java/avdinformatica/group1/rentmycar/utils/Helper.java
@@ -2,23 +2,28 @@ package avdinformatica.group1.rentmycar.utils;
 
 import java.lang.*;
 import java.util.*;
+import java.util.UUID;
 
 public class Helper {
-    public static String[] getLatLong(String coordinates)
-    {
+    public static String[] getLatLong(String coordinates) {
         String[] latLong = coordinates.split(",");
         return latLong;
     }
 
-    public static String getLat(String coordinates)
-    {
+    public static String getLat(String coordinates) {
         String[] latLong = coordinates.split(",");
         return latLong[0];
     }
 
-    public static String getLong(String coordinates)
-    {
+    public static String getLong(String coordinates) {
         String[] latLong = coordinates.split(",");
         return latLong[1];
     }
+
+
+    public static String generateRandomSessionString() {
+        String uuid = UUID.randomUUID().toString();
+        return uuid;
+    }
+
 }

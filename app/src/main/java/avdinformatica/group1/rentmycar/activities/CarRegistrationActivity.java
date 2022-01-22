4439c23f1146eaeea40d1c9096ec911d4897a0f1
Robbert <robbertbode@gmail.com>
2022-01-22 13:55:06 +0100

    Fixes syntax and error messages.

--- app/src/main/java/avdinformatica/group1/rentmycar/activities/CarRegistrationActivity.java
+++ app/src/main/java/avdinformatica/group1/rentmycar/activities/CarRegistrationActivity.java
@@ -5,6 +5,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,24 +16,26 @@ public class CarRegistrationActivity extends AppCompatActivity {
 
     EditText brand;
     EditText model;
-    EditText year;
-    EditText engineType;
-    EditText mileage;
-    Button register;
+    RadioButton engineType;
+    EditText licensePlate;
+    EditText registerLocation;
+    Button btn_car_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_registration);
 
-//        brand = (EditText) findViewById(R.id.editText1);
-//        model = (EditText) findViewById(R.id.editText2);
-//        year = (EditText) findViewById(R.id.editText3);
-//        engineType = (EditText) findViewById(R.id.editText4);
-//        mileage = (EditText) findViewById(R.id.editText5);
-        register = (Button) findViewById(R.id.btn_car_register);
+        brand = (EditText) findViewById(R.id.et_car_register_brand);
+        model = (EditText) findViewById(R.id.et_car_register_model);
+        licensePlate = (EditText) findViewById(R.id.et_car_register_licence_plate);
+        engineType = (RadioButton) findViewById(R.id.rb_car_register_bev);
+        engineType = (RadioButton) findViewById(R.id.rb_car_register_ice);
+        engineType = (RadioButton) findViewById(R.id.rb_car_register_fcev);
+        registerLocation = (EditText) findViewById(R.id.et_car_register_location);
+        btn_car_register = (Button) findViewById(R.id.btn_car_register);
 
-        register.setOnClickListener(new View.OnClickListener() {
+        btn_car_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 checkDataEntered();
@@ -46,27 +49,26 @@ public class CarRegistrationActivity extends AppCompatActivity {
     }
 
     void checkDataEntered() {
-        if (isEmpty(brand)) {
-            Toast t = Toast.makeText(this, "You must enter the car's brand to register!", Toast.LENGTH_SHORT);
-            t.show();
+            if (isEmpty(brand)) {
+                Toast t = Toast.makeText(this, "You must enter the car's brand to register!", Toast.LENGTH_SHORT);
+                t.show();
+            }
+
+            if (isEmpty(model) == false) {
+                model.setError("Your car's year of construction is required!");
+
+            if (isEmpty(licensePlate) == false) {
+                licensePlate.setError("Your car's licenseplate is required!");
+            }
+
+            if (!engineType.isChecked()) {
+                engineType.setError("Your car's engine type is required!");
+            }
+
+            if (isEmpty(registerLocation) == false) {
+                registerLocation.setError("Your car's average mileage is required!");
+            }
+
         }
-
-        if (isEmpty(model)) {
-            model.setError("Car model is required!");
-        }
-
-        if (isEmpty(year) == false) {
-            year.setError("Your car's year of construction is required!");
-        }
-
-        if (isEmpty(engineType) == false) {
-            engineType.setError("Your car's engine type is required!");
-        }
-
-        if (isEmpty(mileage) == false) {
-            mileage.setError("Your car's average mileage is required!");
-        }
-
-
     }
 }

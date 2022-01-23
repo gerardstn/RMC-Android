b8494afe2375ba4ae6a2f0beb4d59b77ddc2b101
Danny van Os <dannyvanos93@gmail.com>
2022-01-23 21:13:58 +0100

    Added GPS service and get it in MainActivity

--- app/src/main/java/avdinformatica/group1/rentmycar/utils/Helper.java
+++ app/src/main/java/avdinformatica/group1/rentmycar/utils/Helper.java
@@ -1,5 +1,7 @@
 package avdinformatica.group1.rentmycar.utils;
 
+import android.location.Location;
+
 import java.lang.*;
 import java.util.*;
 import java.util.UUID;
@@ -10,16 +12,22 @@ public class Helper {
         return latLong;
     }
 
-    public static String getLat(String coordinates) {
+    public static double getLat(String coordinates) {
         String[] latLong = coordinates.split(",");
-        return latLong[0];
+        return Double.parseDouble(latLong[0]);
     }
 
-    public static String getLong(String coordinates) {
+    public static double getLong(String coordinates) {
         String[] latLong = coordinates.split(",");
-        return latLong[1];
+        return Double.parseDouble(latLong[1]);
     }
 
+    /* Returns the units in meters. */
+    public static double calculateDistance(double startLatitude, double startLongitude, double endLatitude, double endLongitude) {
+        float[] results = new float[3];
+        Location.distanceBetween(startLatitude, startLongitude, endLatitude, endLongitude, results);
+        return results[0];
+    }
 
     public static String generateRandomSessionString() {
         String uuid = UUID.randomUUID().toString();

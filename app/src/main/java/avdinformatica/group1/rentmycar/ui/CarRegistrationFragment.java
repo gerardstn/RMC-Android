1356babb28c606ea2e279d08569ac9dec86d8925
Gerard <g.sintnicolaas@student.avans.nl>
2022-01-23 22:38:37 +0100

    a few small adjustments to the homepage, adding km text next to distance, different car listview. todo for detailpage: link data, add simple reserve car post request. todo for distance: add searchfilter on distance. todo for camera: save photo somewhere.

--- app/src/main/java/avdinformatica/group1/rentmycar/ui/CarRegistrationFragment.java
+++ app/src/main/java/avdinformatica/group1/rentmycar/ui/CarRegistrationFragment.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import avdinformatica.group1.rentmycar.R;
 
@@ -16,6 +21,11 @@ import avdinformatica.group1.rentmycar.R;
  * create an instance of this fragment.
  */
 public class CarRegistrationFragment extends Fragment {
+
+    EditText brand, model, licensePlate, registerLocation;
+    RadioButton engineType;
+    Button btn_car_register;
+
 
     public CarRegistrationFragment() {
         // Required empty public constructor
@@ -31,6 +41,8 @@ public class CarRegistrationFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
@@ -39,5 +51,37 @@ public class CarRegistrationFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_car_registration, container, false);
         return view;
+    }
+
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    void checkDataEntered() {
+        if (isEmpty(brand)) {
+            Toast t = Toast.makeText(getActivity().getApplicationContext(), "You must enter the car's brand to register!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+        if (isEmpty(model) == false) {
+            model.setError("Your car's year of construction is required!");
+
+            if (isEmpty(licensePlate) == false) {
+                licensePlate.setError("Your car's licenseplate is required!");
+            }
+
+            if (!engineType.isChecked()) {
+                engineType.setError("Your car's engine type is required!");
+            }
+
+            if (isEmpty(registerLocation) == false) {
+                registerLocation.setError("Your car's average mileage is required!");
+            }
+
+        }
+
+
     }
 }

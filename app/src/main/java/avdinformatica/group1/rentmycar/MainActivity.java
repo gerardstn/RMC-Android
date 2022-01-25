855ab1496ce1bb8a3bd3a3e2874f24076cf211cc
Danny van Os <dannyvanos93@gmail.com>
2022-01-25 22:53:23 +0100

    Remove the alertDialog for now
    This isn't possible for me to fix this at short notice..

--- app/src/main/java/avdinformatica/group1/rentmycar/MainActivity.java
+++ app/src/main/java/avdinformatica/group1/rentmycar/MainActivity.java
@@ -1,16 +1,19 @@
 package avdinformatica.group1.rentmycar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import avdinformatica.group1.rentmycar.services.GPSService;
 import avdinformatica.group1.rentmycar.utils.PermissionHandler;
 
 public class MainActivity extends AppCompatActivity {
+
+    public AppBarConfiguration appBarConfiguration;
+    public NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setIcon(R.drawable.rmclogo);
-
 
     }
 
@@ -42,67 +44,67 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
-        // Create the object of
-        // AlertDialog Builder class
-        AlertDialog.Builder builder
-                = new AlertDialog
-                .Builder(MainActivity.this);
-
-        // Set the message show for the Alert time
-        builder.setMessage("Do you want to exit ?");
-
-        // Set Alert Title
-        builder.setTitle("Alert !");
-
-        // Set Cancelable false
-        // for when the user clicks on the outside
-        // the Dialog Box then it will remain show
-        builder.setCancelable(false);
-
-        // Set the positive button with yes name
-        // OnClickListener method is use of
-        // DialogInterface interface.
-
-        builder
-                .setPositiveButton(
-                        "Yes",
-                        new DialogInterface
-                                .OnClickListener() {
-
-                            @Override
-                            public void onClick(DialogInterface dialog,
-                                                int which) {
-
-                                // When the user click yes button
-                                // then app will close
-                                finish();
-                            }
-                        });
-
-        // Set the Negative button with No name
-        // OnClickListener method is use
-        // of DialogInterface interface.
-        builder
-                .setNegativeButton(
-                        "No",
-                        new DialogInterface
-                                .OnClickListener() {
-
-                            @Override
-                            public void onClick(DialogInterface dialog,
-                                                int which) {
-
-                                // If user click no
-                                // then dialog box is canceled.
-                                dialog.cancel();
-                            }
-                        });
-
-        // Create the Alert dialog
-        AlertDialog alertDialog = builder.create();
-
-        // Show the Alert Dialog box
-        alertDialog.show();
+//        // Create the object of
+//        // AlertDialog Builder class
+//        AlertDialog.Builder builder
+//                = new AlertDialog
+//                .Builder(MainActivity.this);
+//
+//        // Set the message show for the Alert time
+//        builder.setMessage("Do you want to exit ?");
+//
+//        // Set Alert Title
+//        builder.setTitle("Alert !");
+//
+//        // Set Cancelable false
+//        // for when the user clicks on the outside
+//        // the Dialog Box then it will remain show
+//        builder.setCancelable(false);
+//
+//        // Set the positive button with yes name
+//        // OnClickListener method is use of
+//        // DialogInterface interface.
+//
+//        builder
+//                .setPositiveButton(
+//                        "Yes",
+//                        new DialogInterface
+//                                .OnClickListener() {
+//
+//                            @Override
+//                            public void onClick(DialogInterface dialog,
+//                                                int which) {
+//
+//                                // When the user click yes button
+//                                // then app will close
+//                                finish();
+//                            }
+//                        });
+//
+//        // Set the Negative button with No name
+//        // OnClickListener method is use
+//        // of DialogInterface interface.
+//        builder
+//                .setNegativeButton(
+//                        "No",
+//                        new DialogInterface
+//                                .OnClickListener() {
+//
+//                            @Override
+//                            public void onClick(DialogInterface dialog,
+//                                                int which) {
+//
+//                                // If user click no
+//                                // then dialog box is canceled.
+//                                dialog.cancel();
+//                            }
+//                        });
+//
+//        // Create the Alert dialog
+//        AlertDialog alertDialog = builder.create();
+//
+//        // Show the Alert Dialog box
+//        alertDialog.show();
 
     }
 }

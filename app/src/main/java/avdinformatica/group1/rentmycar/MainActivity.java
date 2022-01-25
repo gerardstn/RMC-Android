083ad81345f65390ad719fcf628ef9a4fa357100
Danny van Os <dannyvanos93@gmail.com>
2022-01-26 00:29:49 +0100

    Updated the ActionBar

--- app/src/main/java/avdinformatica/group1/rentmycar/MainActivity.java
+++ app/src/main/java/avdinformatica/group1/rentmycar/MainActivity.java
@@ -1,13 +1,20 @@
 package avdinformatica.group1.rentmycar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import avdinformatica.group1.rentmycar.services.GPSService;
+import avdinformatica.group1.rentmycar.ui.LoginFragment;
 import avdinformatica.group1.rentmycar.utils.PermissionHandler;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,11 +27,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         PermissionHandler.permissionCheck(MainActivity.this);
         setContentView(R.layout.activity_main);
-
         GPSService mGPS = new GPSService(this);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.rmclogo);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        if (navHostFragment != null) {
+
+            NavController navController = navHostFragment.getNavController();
+            AppBarConfiguration appBarConfiguration =
+                    new AppBarConfiguration.Builder(navController.getGraph()).build();
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            NavigationUI.setupActionBarWithNavController(
+                    this, navController, appBarConfiguration);
+        }
+
+        Fragment f = getSupportFragmentManager().findFragmentById(R.id.fragment_login);
+        if(f instanceof LoginFragment){
+            System.out.println("Current fragment is the one to show the AlertDialog");
+        }else{
+            System.out.println("No..");
+        }
+//        getSupportActionBar().setIcon(R.drawable.rmclogo);
 
     }
 
@@ -38,7 +61,76 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onUserLeaveHint(){
+        // Create the object of
+        // AlertDialog Builder class
+        AlertDialog.Builder builder
+                = new AlertDialog
+                .Builder(MainActivity.this);
+
+        // Set the message show for the Alert time
+        builder.setMessage("Do you want to exit ?");
+
+        // Set Alert Title
+        builder.setTitle("Alert !");
+
+        // Set Cancelable false
+        // for when the user clicks on the outside
+        // the Dialog Box then it will remain show
+        builder.setCancelable(false);
+
+        // Set the positive button with yes name
+        // OnClickListener method is use of
+        // DialogInterface interface.
+
+        builder
+                .setPositiveButton(
+                        "Yes",
+                        new DialogInterface
+                                .OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+
+                                // When the user click yes button
+                                // then app will close
+                                finish();
+                            }
+                        });
+
+        // Set the Negative button with No name
+        // OnClickListener method is use
+        // of DialogInterface interface.
+        builder
+                .setNegativeButton(
+                        "No",
+                        new DialogInterface
+                                .OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+
+                                // If user click no
+                                // then dialog box is canceled.
+                                dialog.cancel();
+                            }
+                        });
+
+        // Create the Alert dialog
+        AlertDialog alertDialog = builder.create();
+
+        // Show the Alert Dialog box
+        alertDialog.show();
+    }
+
+    @Override
     public void onBackPressed() {
+
+        System.out.println("Current Fragment: " + getSupportFragmentManager().findFragmentById(R.id.fragment_login));
+        System.out.println("Back stack count: " + getFragmentManager().getBackStackEntryCount());
+
         if (getFragmentManager().getBackStackEntryCount() > 0) {
             getFragmentManager().popBackStack();
         } else {
